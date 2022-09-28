9d3bebecc1535035b80fe97a97921a31d70562c6
Simorr98 <ramosrayosimo@gmail.com>
2022-09-28 10:37:07 +0200

    Añado el archivo arrays.php

---
+++ arrays.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<body>
+<?php
+//Arrays
+$dias = array("Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo");
+$temperaturas = [23,14,31,10];
+
+echo $dias[1];
+echo "<br>";
+echo $temperaturas[0];
+echo "<br>";
+echo "<br>";
+var_dump($dias);
+echo "<br>";
+var_dump($temperaturas);
+?>
+</body>
+
+</html>
