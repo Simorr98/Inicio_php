3969894b5a871b52bb8ca02c150f8a3ecde15461
Simorr98 <ramosrayosimo@gmail.com>
2022-09-28 10:19:02 +0200

    Añado el archivo destino.php y pasovariables.php

---
+++ destino.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<body>
+<?php
+
+echo "Nombre: " . $_GET["nombre"];
+echo "<p> </p>";
+echo "Cargo: " . $_GET["cargo"];
+
+?>
+</body>
+
+</html>
---
+++ pasovariables.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<body>
+    //Sirve para pasar variables a otro .php
+    <a href="destino.php?nombre=Luis&cargo=Profesor"> Pasar las variables a destino.php</a>
+</body>
+
+</html>
